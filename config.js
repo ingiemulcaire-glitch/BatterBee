b35741757738e6d2d3ffd58b7da27fbc7251c0d3
ingiemulcaire-glitch <ingiemulcaire@gmail.com>
2026-07-03 13:50:50 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,18 @@
-{}
+module.exports = {
+  // Discord
+  clientId: "1519022460563882217",
+  guildId: "1512899816626323577",
+
+  // Channels
+  verifyChannel: "1513073038789316660",
+  logChannel: "1513388178973921341",
+
+  // Roles
+  verifiedRole: "1513897216329121792",
+
+  // Embed
+  embedColor: "#1c1d23",
+
+  // Nickname Format
+  nicknameFormat: "𐔌 ⋮ {discord} .ᐟ {roblox}"
+};
